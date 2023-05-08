4edc1c00521717f0a45bac31a1bb4707a820e619
Carlos Jasso <contact@carlosjasso.dev>
2023-05-08 13:40:10 -0600

    updated site's configurations for minimal functionality

--- conf.py
+++ conf.py
@@ -4,13 +4,19 @@ SITENAME = "Mad Coders"
 PATH = "articles"
 DEFAULT_LANG = "en"
 
-ARTICLE_OUTPUT_PATH = "articles"
+ARTICLE_OUTPUT_PATH = PATH
 ARTICLE_SAVE_AS = f"{ARTICLE_OUTPUT_PATH}/{{slug}}.html"
+
+STATIC_PATHS = ["static"]
 
 # Disable not needed pages
 AUTHOR_SAVE_AS = ""
+AUTHORS_SAVE_AS = ""
 CATEGORY_SAVE_AS = ""
+CATEGORIES_SAVE_AS = ""
 TAG_SAVE_AS = ""
+TAGS_SAVE_AS = ""
+ARCHIVES_SAVE_AS = ""
 
 # Disable feed generation
 TIMEZONE = "America/Mexico_City"
@@ -21,7 +27,13 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Ignore example files
-IGNORE_FILES = ["*.example.*"]
+IGNORE_FILES = [
+    "*.example.*",
+    "*.psd",
+    "*.scss"
+]
 
 # Theme configuration
 THEME = "./theme"
+THEME_STATIC_DIR = "resources"
+THEME_STATIC_PATHS = [THEME_STATIC_DIR]
