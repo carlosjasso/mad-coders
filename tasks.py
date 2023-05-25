b8b06ded7d052d107424530ace1d7d3552de7cc8
Carlos Jasso <contact@carlosjasso.dev>
2023-05-25 11:07:50 -0600

    refactored tasks for simplicity

--- tasks.py
+++ tasks.py
@@ -1,16 +1,17 @@
 import subprocess
 import sys
 import typing
-from argparse import SUPPRESS, ArgumentParser, Namespace
+from abc import ABC
+from argparse import SUPPRESS, ArgumentParser, Namespace, _SubParsersAction
 from dataclasses import dataclass, field
 from datetime import datetime, timezone
+from inspect import signature
 from os import path
 
 import conf_base
 import conf_prod
 
 # region constants
-
 # python required version based on pelican requirements
 # info: https://docs.getpelican.com/en/latest/quickstart.html#installation
 REQUIRED_PYTHON_VERSION_MAJOR = 3
@@ -24,30 +25,53 @@ ParserHandler = typing.Callable[[Namespace], None]
 
 
 @dataclass(init=False, frozen=True)
-class Attributes:
+class Terms:
     ParserHandler = "parserHandler"
     IsProd = "isProd"
     Title = "title"
     Status = "status"
-    Statuses = ["draft", "published", "hidden"]
 
 
 @dataclass
-class ParserOption:
-    arg: str
-    dest: str
+class ParserArgument:
+    flag: str
+    name: str
     help: str
     action: str  # info: https://docs.python.org/3/library/argparse.html#action
     required: bool = False
-    choices: typing.List[typing.Any] = None
+    choices: typing.List[typing.Any] = field(default_factory=list)
 
 
-@dataclass()
-class Parser:
+@dataclass
+class ParserBase(ABC):
     name: str
     description: str
-    handler: ParserHandler
-    options: typing.List[ParserOption] = field(default_factory=list)
+    handler: ParserHandler = None
+    arguments: typing.List[ParserArgument] = field(default_factory=list)
+
+
+@dataclass
+class SubParser(ParserBase):
+    pass
+
+
+@dataclass
+class Parser(ParserBase):
+    subparsers: typing.List[SubParser] = field(default_factory=list)
+
+
+@dataclass
+class Article():
+    title: str
+    date: str
+    modified: str
+    tags: str
+    slug: str
+    author: str
+    summary: str
+    status: str
+    basename: str
+    path: str
 # endregion
 
 
@@ -55,110 +79,128 @@ def main(args: typing.List[str]) -> None:
     if not __isPythonVersionvalid():
         exit(f"{path.basename(__file__)} requires python version {REQUIRED_PYTHON_VERSION_MAJOR}.{REQUIRED_PYTHON_VERSION_MINOR}+ ro tun.")
 
-    parser = __buildArgumentParser()
+    parser = __initArgumentParser()
 
     if not any(args):
         parser.print_help()
         exit()
 
-    namespace = parser.parse_args(args)
-    handler: ParserHandler = getattr(namespace, Attributes.ParserHandler)
-    handler(namespace)
+    __handlerParser(parser, args)
 
 
-def __isPythonVersionvalid() -> bool:
-    installed = (sys.version_info.major, sys.version_info.minor)
-    required = (REQUIRED_PYTHON_VERSION_MAJOR, REQUIRED_PYTHON_VERSION_MINOR)
+# region argument parser init
+def __initArgumentParser() -> ArgumentParser:
+    rootParser = ArgumentParser(
+        description="Executes automated tasks of the project."
+    )
 
-    return installed >= required
+    rootContainer = rootParser.add_subparsers()
+
+    for parserObject in __getParsers():
+        parser = __attachSubParser(rootContainer, parserObject)
+
+        if any(parserObject.subparsers):
+            container = parser.add_subparsers()
+            for subParserObject in parserObject.subparsers:
+                __attachSubParser(container, subParserObject)
+
+    return rootParser
 
 
-def __buildArgumentParser() -> ArgumentParser:
-    parser = ArgumentParser(
-        description="Executes automated tasks of the project.")
+def __getParsers() -> typing.List[Parser]:
+    return [
+        __initBuildParser(),
+        __initServeParser(),
+        __initArticleParser()
+    ]
 
-    subparsers = parser.add_subparsers()
 
-    for item in __getSubparsers():
-        subparser = subparsers.add_parser(
-            item.name,
-            help=item.description,
-            description=item.description
-        )
+def __attachSubParser(container: _SubParsersAction, parserObject: Parser) -> ArgumentParser:
+    parser: ArgumentParser = container.add_parser(
+        parserObject.name,
+        **__getParserArgsDict(parserObject)
+    )
 
-        subparser.add_argument(
-            "--parser-handler",
-            dest=Attributes.ParserHandler,
-            type=ParserHandler,
-            default=item.handler,
-            help=SUPPRESS,
-        )
-
-        for option in item.options:
-            kargs = {
-                "dest": option.dest,
-                "help": option.help,
-                "action": option.action
-            }
-
-            if (option.choices is not None and any(option.choices)):
-                kargs["choices"] = option.choices
-
-            subparser.add_argument(
-                option.arg,
-                **kargs
-            )
+    __attachParcerArguments(
+        parser,
+        parserObject.handler,
+        parserObject.arguments
+    )
 
     return parser
 
 
-def __getSubparsers() -> typing.List[Parser]:
-    return [
-        Parser(
-            name="build",
-            description="Builds the project and generates the site content",
-            handler=__handleBuildParser,
-            options=[
-                ParserOption(
-                    arg="--is-prod",
-                    dest=Attributes.IsProd,
-                    help="If included, the site content will be generated in the production directory",
-                    action="store_true"
-                )
-            ]
-        ),
-        Parser(
-            name="serve",
-            description="Starts the development environment server",
-            handler=__handleServeParser
-        ),
-        Parser(
-            name="generate",
-            description="Generates a new time-stampped article file.",
-            handler=__handleGenerateParser,
-            options=[
-                ParserOption(
-                    arg="--title",
-                    dest=Attributes.Title,
-                    help="The new article title. It will also be used as fileaname (Spaces will be replaced with hyphens).",
-                    action="store",
-                    required=True
-                ),
-                ParserOption(
-                    arg="--status",
-                    dest=Attributes.Status,
-                    help="The published status for the new article (defaults to \"draft\").",
-                    action="store",
-                    required=False,
-                    choices=Attributes.Statuses
-                )
-            ]
+def __attachParcerArguments(parser: ArgumentParser, handler: ParserHandler, arguments: typing.List[ParserArgument]) -> None:
+    parser.add_argument(
+        "--parser-handler",
+        **__getParserHandlerArgsDict(handler or parser.print_help)
+    )
+
+    for argument in arguments:
+        parser.add_argument(
+            argument.flag,
+            **__getParserArgumentArgsDict(argument)
         )
-    ]
 
 
-def __handleBuildParser(namespance: Namespace) -> None:
-    isProd = getattr(namespance, Attributes.IsProd, False)
+def __initBuildParser() -> Parser:
+    return Parser(
+        name="build",
+        description="Builds the project and generates the site content",
+        handler=__handleBuildParser,
+        arguments=[
+            ParserArgument(
+                flag="--is-prod",
+                name=Terms.IsProd,
+                help="If included, the site content will be generated in the production directory",
+                action="store_true"
+            )
+        ]
+    )
+
+
+def __initServeParser() -> Parser:
+    return Parser(
+        name="serve",
+        description="Starts the development environment server",
+        handler=__handleServeParser
+    )
+
+
+def __initArticleParser() -> Parser:
+    return Parser(
+        name="article",
+        description="Utility tasks for article creation and manipulation.",
+        subparsers=[
+            SubParser(
+                name="new",
+                description="Create a new scaffolded article file.",
+                handler=__handleNewArticleParser,
+                arguments=[
+                    ParserArgument(
+                        flag="--title",
+                        name=Terms.Title,
+                        help="The new article title. It will also be used as fileaname (Spaces will be replaced with hyphens).",
+                        action="store",
+                        required=True
+                    ),
+                    ParserArgument(
+                        flag="--status",
+                        name=Terms.Status,
+                        help="The published status for the new article (defaults to \"draft\").",
+                        action="store",
+                        choices=["draft", "published", "hidden"]
+                    )
+                ]
+            )
+        ]
+    )
+# endregion
+
+
+# region argument parser handlers
+def __handleBuildParser(namespace: Namespace) -> None:
+    isProd = getattr(namespace, Terms.IsProd, False)
     options = ["--ignore-cache"] if isProd else []
     conf = conf_prod if isProd else conf_base
     __runPelicanCommand(options, conf)
@@ -168,37 +210,105 @@ def __handleServeParser(_: Namespace) -> None:
     __runPelicanCommand(options=["--listen", "--autoreload"])
 
 
-def __handleGenerateParser(namespance: Namespace) -> None:
+def __handleNewArticleParser(namespace: Namespace) -> None:
     title = str(
-        getattr(namespance, Attributes.Title, "")
+        getattr(namespace, Terms.Title, "")
     ).strip()
+
     status = str(
-        getattr(namespance, Attributes.Status, "draft")
+        getattr(namespace, Terms.Status, "draft")
     )
 
+    article = __initArticle(title, status)
+
+    with open(article.path, mode="x") as file:
+        frontmatter = __getFrontMatter(article)
+        file.write(frontmatter)
+
+    __runCommand(f"code {article.path} --reuse-window")
+# endregion
+
+
+# region helpers
+def __isPythonVersionvalid() -> bool:
+    installed = (sys.version_info.major, sys.version_info.minor)
+    required = (REQUIRED_PYTHON_VERSION_MAJOR, REQUIRED_PYTHON_VERSION_MINOR)
+
+    return installed >= required
+
+
+def __getParserArgsDict(parser: ParserBase) -> typing.Dict[str, str]:
+    return {
+        "help": parser.description,
+        "description": parser.description
+    }
+
+
+def __getParserHandlerArgsDict(handler: ParserHandler) -> typing.Dict[str, typing.Any]:
+    return {
+        "dest": Terms.ParserHandler,
+        "type": ParserHandler,
+        "default": handler,
+        "help": SUPPRESS,
+    }
+
+
+def __getParserArgumentArgsDict(argument: ParserArgument) -> typing.Dict[str, typing.Any]:
+    value = {
+        "dest": argument.name,
+        "help": argument.help,
+        "action": argument.action,
+        "required": argument.required
+    }
+
+    if any(argument.choices):
+        value["choices"] = argument.choices
+
+    return value
+
+
+def __handlerParser(parser: ArgumentParser, args: typing.List[str]) -> None:
+    namespace = parser.parse_args(args)
+    handler: function = getattr(namespace, Terms.ParserHandler)
+    if "argparse.Namespace" in str(signature(handler)):
+        handler(namespace)
+    else:
+        handler()
+
+
+def __initArticle(title: str, status: str) -> Article:
     utcTime = datetime.now(timezone.utc)
-    date = utcTime.strftime('%Y-%m-%d')
+    timestamp = utcTime.strftime('%Y-%m-%d %H:%M')
     slug = title.replace(' ', '-')
-    fileName = f"{date}_{slug}.md"
-    filePath = path.join(WORKSPACE_PATH, conf_base.PATH, fileName)
+    basename = f"{utcTime.strftime('%Y-%m-%d')}_{slug}.md"
 
-    timeStamp = utcTime.strftime('%Y-%m-%d %H:%M')
-    with open(filePath, mode="x") as file:
-        lines = "\n".join([
-            "---",
-            f"title: {title}",
-            f"date: {timeStamp}",
-            f"modified: {timeStamp}",
-            f"tags: none",
-            f"slug: {slug}",
-            f"author: {conf_base.SITENAME}",
-            f"summary: {title}",
-            f"status: {status}",
-            "---"
-        ]) + ("\n" * 2)
-        file.write(lines)
+    return Article(
+        title=title,
+        status=status,
+        date=timestamp,
+        modified=timestamp,
+        tags=None,
+        slug=slug,
+        author=conf_base.SITENAME,
+        summary=title,
+        basename=basename,
+        path=path.join(WORKSPACE_PATH, conf_base.PATH, basename)
+    )
 
-    __runCommand(f"code {filePath} --reuse-window")
+
+def __getFrontMatter(article: Article) -> str:
+    return "\n".join([
+        "---",
+        f"title: {article.title}",
+        f"date: {article.date}",
+        f"modified: {article.modified}",
+        f"tags: {article.tags or 'None'}",
+        f"slug: {article.slug}",
+        f"author: {article.author}",
+        f"summary: {article.summary}",
+        f"status: {article.status}",
+        "---"
+    ]) + ("\n" * 2)
 
 
 def __runPelicanCommand(options: typing.List[str], conf=conf_base):
@@ -213,6 +323,7 @@ def __runPelicanCommand(options: typing.List[str], conf=conf_base):
 
 def __runCommand(command: str):
     subprocess.run(command.strip(), shell=True)
+# endregion
 
 
 if __name__ == "__main__":
